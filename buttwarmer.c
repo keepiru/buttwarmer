36a6f803ecaa730e226b8a9594fb71c87859dd81
Chris Kai Frederick <git@neofin.net>
2025-02-09 18:21:07 -0800

    Improve documentation

--- buttwarmer.c
+++ buttwarmer.c
@@ -19,21 +19,31 @@
 #define MILLIVOLTS_OFF 1000
 
 ISR(ADC_vect, ISR_NAKED) { asm("reti"); }
-ISR(__vector_default) { puts_kai("beep"); }
+ISR(__vector_default) { puts_kai("beep"); }  // This shouldn't happen. :)
 
 int putchr(char, FILE *);
 FILE mystdout = FDEV_SETUP_STREAM(putchr, NULL, _FDEV_SETUP_WRITE);
 
+/*
+ * @brief initialize the serial port.
+ */
 inline void uart_init(void) {
 #define BAUD 2400
 #include <util/setbaud.h>
-	OSCCAL = 0x89;
-	UBRR0 = UBRR_VALUE;
+	OSCCAL = 0x89;       // Oscillator calibration
+	UBRR0 = UBRR_VALUE;  // Set the baud rate.  This comes from the included lib.
 	UCSR0B |= 1<<TXEN0 ; // Enable TX
 	stdout = &mystdout;
 } // void uart_init
 
-int putchr(char c, FILE *stream) { // stdio.h wants this to be public
+/*
+ * @brief Output a single character to the serial port.
+ * @note stdio.h wants this to be public
+ * @param[in] c the character to print.
+ * @param[in] stream The theoretical stream.  Not actually used.
+ * @return 0 This implementation will block until it succeeds.
+ */
+int putchr(char c, FILE *stream) {
 	if (c == '\n')
 		putchr('\r', stream);
 	loop_until_bit_is_set(UCSR0A, UDRE0);
@@ -41,23 +51,35 @@ int putchr(char c, FILE *stream) { // stdio.h wants this to be public
 	return 0;
 } // void putchr
 
+/*
+ * @brief Initialize the hardware PWMs.
+ */
 inline void pwm_init(void) {
 	TCCR0A = 1<<COM0A1 | 1<<COM0B1 | 1<<WGM00 ; // PWM enable, mode
 	TCCR1A = 1<<COM1A1 | 1<<COM1B1 | 1<<WGM10 ; // PWM enable, mode
 	TCCR0B = 1<<CS00 | 1<<CS01;                 // frequency
 	TCCR1B = 1<<CS10 | 1<<CS11;                 // frequency
-	TCNT1 = 0;
-	TCNT0 = 128;
+	TCNT1 = 0;                                  // initialize the counter
+	TCNT0 = 128;                                // And this one is on the alternate phase
 	DDRD |= 1<<5 | 1<<6;
 	DDRB |= 1<<1 | 1<<2;
 } // void pwm_init
 
+/*
+ * @brief Sample an ADC pin.
+ * @param[in] pin The analog pin number to sample.
+ * @param[in] ref A bitfield for the voltage reference. Typically:
+ *                1<<REFS0              VCC
+ *                1<<REFS0 | 1<<REFS1   Internal 1.1V voltage reference
+ *                FIXME: This can easily be abstracted.
+ * @return The raw sample in ADC counts.
+ */
 uint16_t adc_sample(uint8_t pin, uint8_t ref) { // sample pin with reference voltage bitfield ref
-        ADCSRA = 1<<ADEN | 1<<ADIE | 4 ; // enable, int, prescaler
-        SMCR = 3 ;                       // Sleep mode ADC, enable
-        ADMUX = ref | pin ;
-	_delay_ms(10);
-	sleep_mode();
+	ADCSRA = 1<<ADEN | 1<<ADIE | 4 ; // enable, int, prescaler
+	SMCR = 3 ;                       // Sleep mode ADC, enable
+	ADMUX = ref | pin ;              // Where to sample from
+	_delay_ms(10);                   // Take a moment to settle
+	sleep_mode();                    // Enter ADC sleep mode, wait for irq
 	return ADCW;
 }
 
@@ -65,27 +87,38 @@ uint16_t adc_sample(uint8_t pin, uint8_t ref) { // sample pin with reference vol
  * @brief Sample analog pin and update output PWM.
  * @note This is the heart of the main loop.
  * @param[in] pin The analog input pin to sample.  This is the pin connected to the potentiometer.
- * @param[in] lockout_pin Power input sent through a voltage divider, used to measure battery level.
+ * @param[in] lockout_pin Each seat may be turned on or off.  Disable PWMs if this seat is off.
  * @param[out] port Pointer to the PWM output register.
  */
 void pwm_update(uint8_t pin, uint8_t lockout_pin, volatile uint8_t *port) {
 	int16_t old;
 	uint16_t knob, voltage;
-	knob = adc_sample(pin, 1<<REFS0) / 4;
+	knob = adc_sample(pin, 1<<REFS0) / 4;  // divide by 4 from 10-bit ADC to 8 bit PWM
 	voltage = adc_sample(lockout_pin, 1<<REFS0|1<<REFS1) * MILLIVOLTS_PER_DIV;
 	old = *port;
+
+	// Add some hysteresis.  Ignore small changes.
 	if ( abs(knob-old) > HYST ) {
 		*port = knob;
-		//printf_kai("a%d>%d\r\n", pin, sample);
 	}
+
+	// Add a small dead zone at the top and bottom of the potentiometer.
+	// The deadband is the same size as the hysteresis width.
 	if (knob < HYST) *port = 0;
 	if (knob > 255-HYST)  *port = 255;
+
+	// If the switch for this side is off, ignore the knob positions and
+	// just disable this output.
 	if (voltage < MILLIVOLTS_OFF) *port = 0;
 }
 
+/*
+ * @brief Turn off all outputs and halt the system.
+ * @note This is typically done when the battery is getting too low.
+ */
 void shutdown(void) { // set all outputs low and halt
 	puts_kai("Shutdown");
-	while (1) { 
+	while (1) {
 		OCR0A=0;
 		OCR0B=0;
 		OCR1A=0;
@@ -103,9 +136,12 @@ void shutdown(void) { // set all outputs low and halt
 	}
 }
 
+/*
+ * @brief Monitor the battery voltage, and shut down if battery is low
+ */
 void monitor_voltage(void) { // sample voltage, maintain decaying average, shut down if too low
 	static uint16_t millivolts_avg=13500;
-	uint16_t sample;
+	uint16_t sample; // The sampled value in millivolts.
 	// Power is sourced from either seat circuit.  We monitor those
 	// independently because one may be switched off, making it look like
 	// the battery is dead.  High PWM levels also cause some voltage drop.
@@ -117,7 +153,9 @@ void monitor_voltage(void) { // sample voltage, maintain decaying average, shut 
 	);
 	millivolts_avg = (millivolts_avg * (1-DECAY_RATE)) + (sample * DECAY_RATE); // decaying average
 	printf_kai("v:%u %u\r", sample, millivolts_avg);
-	if (millivolts_avg < MILLIVOLTS_SHUTDOWN) shutdown();
+	if (millivolts_avg < MILLIVOLTS_SHUTDOWN) {
+		shutdown();
+	}
 }
 
 int main (void) {
@@ -127,10 +165,10 @@ int main (void) {
 	_delay_ms(100);
 	sei();
 	while(1) {
-		pwm_update(1, 5, &OCR0A);
-		pwm_update(2, 5, &OCR0B);
-		pwm_update(3, 0, &OCR1AL);
-		pwm_update(4, 0, &OCR1BL);
+		pwm_update(1, 5, &OCR0A);  // Left top
+		pwm_update(2, 5, &OCR0B);  // Left bottom
+		pwm_update(3, 0, &OCR1AL); // Right top
+		pwm_update(4, 0, &OCR1BL); // Right bottom
 		monitor_voltage();
 		_delay_ms(200);
 	}
